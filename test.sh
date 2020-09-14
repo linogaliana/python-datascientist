18188589eefc10a9694bea485a77c4f33837db5f
Lino Galiana <linogaliana@yahoo.fr>
2020-09-14 18:18:39 +0200

    correct typo arg

--- test.sh
+++ test.sh
@@ -7,5 +7,5 @@ cd "./content"
 
 # CONVERT INTO NOTEBOOKS AND EXECUTE
 for i in $(ls **/*.md **/*.Rmd | grep -v 'index.md$'); do
-  jupytext --to ipynb -execute "$i"
+  jupytext --to ipynb --execute "$i"
 done
