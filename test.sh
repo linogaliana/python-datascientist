8e78328c5334afcce133d3a1951f7ccef3b383a5
Lino Galiana <linogaliana@yahoo.fr>
2020-09-15 10:50:16 +0200

    test using python script

--- test.sh
+++ test.sh
@@ -7,5 +7,6 @@ cd "./temp"
 
 # CONVERT INTO NOTEBOOKS AND EXECUTE
 for i in $(ls **/*.md **/*.Rmd | grep -v 'index.md$'); do
-  jupytext --to ipynb --execute "$i"
+  jupytext --to py --execute "$i"
+  # jupytext --to ipynb --execute "$i"
 done
