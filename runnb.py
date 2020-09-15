0627b54c7cd3717eaac358b82275d31097ed63f0
Lino Galiana <linogaliana@yahoo.fr>
2020-09-15 10:07:10 +0200

    Functions to clean YAML headers
    Solution to #22

--- runnb.py
+++ runnb.py
@@ -1,28 +1,42 @@
 import os
-import subprocess
-import tempfile
+import glob
 
-import nbformat
+def cleanblog():
+    # LIST (R)MARKDOWN FILES ----------------
+    root_dir = os.getcwd()
+    os.chdir("./content")
+    types = ('**/*.Rmd', '**/*.md')  # the tuple of file types
+    files_grabbed = []
+    for files in types:
+        files_grabbed.extend(glob.glob(files, recursive=True))
+    list_files = []
+    for i in files_grabbed:
+        if "index.md" not in i:
+            list_files = list_files + [i]
+    # APPLY cleanyaml
+    for i in list_files:
+        cleanyaml(i, root_dir)
 
-def _notebook_run(path):
-    """Execute a notebook via nbconvert and collect output.
-       :returns (parsed nb object, execution errors)
-    """
-    dirname, __ = os.path.split(path)
-    os.chdir(dirname)
-    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
-        args = ["nbconvert", "--to", "notebook", "--execute",
-          "--ExecutePreprocessor.timeout=60",
-          "--output", fout.name, path]
-        subprocess.check_call(args)
 
-        fout.seek(0)
-        nb = nbformat.read(fout, nbformat.current_nbformat)
+def cleanyaml(filename, root_dir):
+    # READ MARKDOWN --------------
+    with open(filename, 'r', encoding='utf-8') as f:
+        text = f.readlines()
+        new_text = "".join([line for line in text])
+    s = new_text
+    # EXTRACT AND CLEAN HEADER ----------
+    yaml, text = new_text.split('---\n', 2)[1:]
+    yaml_jupytext, yaml_rmd = yaml.split('title:')
+    new_md = "---\n" + yaml_jupytext.rstrip() + "\n---\n" + text
+    # WRITE IN TEMPORARY LOCATION --------------
+    write_dest = os.path.join(root_dir, "temp" + os.sep + filename)
+    tempdir = write_dest.rsplit(os.sep, 1)[0]
+    if not os.path.exists(tempdir):
+        os.makedirs(tempdir)
+    mode = 'a' if os.path.exists(write_dest) else 'w'
+    with open(write_dest, mode, encoding='utf-8') as f:
+        f.write(new_md)
+    print("Done: " + filename)
 
-    errors = [output for cell in nb.cells if "outputs" in cell
-                     for output in cell["outputs"]\
-                     if output.output_type == "error"]
 
-    return nb, errors
-
-jupytext --to py "D:/W3CRK9/Mes Documents/FORMATIONS/python-datascientist/01_data/01_tp_numpy.ipynb"
+cleanblog()
