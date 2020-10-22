4d945112088a4074830b27c99113ba8b6c7c3f0b
Lino Galiana <linogaliana@yahoo.fr>
2020-10-22 17:42:09 +0200

    more on gitlab ci

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Test automatique des notebooks
 
+set test extension
+
 rm -rf "./temp"
 
 python cleanmd.py
@@ -8,8 +10,20 @@ python cleanmd.py
 
 cd "./temp"
 
-# CONVERT INTO NOTEBOOKS AND EXECUTE
-for i in $(ls **/*.md **/*.Rmd | grep -v 'index.md$'); do
-  jupytext --to py --execute "$i"
-  # jupytext --to ipynb "$i"
-done
+
+if [ "$test" = true ] ; then
+then
+  # CONVERT INTO NOTEBOOKS AND EXECUTE
+  for i in $(ls **/*.md **/*.Rmd | grep -v 'index.md$'); do
+    jupytext --to py --execute "$i"
+  done
+else
+  # Ici :
+  #   * $extension = "md"  -> enonces
+  #   * $extension = "Rmd" -> corrections
+  for i in $(find . -type f -iname "*.$extension"); do
+    # jupytext --to py --execute "$i"
+    jupytext --to ipynb "$i"
+  done
+fi
+
