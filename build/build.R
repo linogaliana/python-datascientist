78472067229674c67f52b45dacafc2d25ce98b21
Lino Galiana <linogaliana@yahoo.fr>
2021-04-27 20:59:35 +0200

    clean html a posteriori

--- build/build.R
+++ build/build.R
@@ -10,3 +10,9 @@ lapply(
     print(sprintf("Rendering %s", i))
     rmarkdown::render(i, envir = new.env())
   })
+
+file.remove(
+  gsub(
+    ".Rmd",".html", list.files("./content", recursive = TRUE, pattern = "*.Rmd", full.names = TRUE)
+  )
+)
