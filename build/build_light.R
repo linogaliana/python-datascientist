d3c695806e38710920e3a70f7060161bd7374bef
linogaliana <linogaliana@yahoo.fr>
2021-07-24 15:52:09 +0000

    retire tpsupp

--- build/build_light.R
+++ build/build_light.R
@@ -1,5 +1,7 @@
 content_rmd <-  list.files("./content", recursive = TRUE, pattern = "*.Rmd", full.names = TRUE)
 content_rmd <- content_rmd[!grepl("/git/", content_rmd)]
+content_rmd <- content_rmd[!grepl("06a_exo_supp_webscraping.md", content_rmd)]
+
 
 file.remove(
   gsub(
