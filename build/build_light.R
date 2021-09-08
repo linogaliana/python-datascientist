1c317ae2a041e904753c9d72ffa84dc6630a0978
linogaliana <linogaliana@yahoo.fr>
2021-09-08 13:42:36 +0000

    change md

--- build/build_light.R
+++ build/build_light.R
@@ -1,7 +1,7 @@
 content_rmd <- list.files("./content", recursive = TRUE, pattern = "*.Rmd", full.names = TRUE)
 content_rmd <- content_rmd[!grepl("/git/", content_rmd)]
 content_rmd <- content_rmd[!grepl("06a_exo_supp_webscraping.", content_rmd)]
-content_rmd <- content_rmd[14]
+content_rmd <- content_rmd[5]
 
 file.remove(
   gsub(
