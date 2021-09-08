4b702429ff3fef91715e340a6bae93a04e401741
linogaliana <linogaliana@yahoo.fr>
2021-09-08 08:23:19 +0000

    un peu de ménage

--- build/wc_website.py
+++ build/wc_website.py
@@ -2,7 +2,9 @@ import glob
 
 list_files = glob.glob("./content/course/**/*.Rmd", recursive=True)
 
+
 filename = list_files[0]
+
 
 with open(filename, encoding='utf-8') as f:
   content = f.readlines()
