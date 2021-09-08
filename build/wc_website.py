bbb2079c40537dec420f6ee63d48daee08530dc5
linogaliana <linogaliana@yahoo.fr>
2021-09-08 11:18:27 +0000

    buils

--- build/wc_website.py
+++ build/wc_website.py
@@ -1,6 +1,8 @@
 import glob
+import matplotlib.pyplot as plt
+import wordcloud
 
-list_files = glob.glob("./content/course/**/*.Rmd", recursive=True)
+list_files = glob.glob("./temp/course/**/*.Rmd", recursive=True)
 
 
 filename = list_files[0]
@@ -9,5 +11,23 @@ filename = list_files[0]
 with open(filename, encoding='utf-8') as f:
   content = f.readlines()
 
+def make_wordcloud(corpus):
+    wc = wordcloud.WordCloud(background_color="white", max_words=2000, contour_width=3, contour_color='steelblue')
+    wc.generate(corpus)
+    return wc
 
-content
+
+def clean_file(text):
+    text = " ".join(text).lower()
+    return text
+
+
+corpus = clean_file(text = content)
+
+fig = plt.figure()
+
+
+plt.imshow(make_wordcloud(corpus), interpolation='bilinear')
+plt.axis("off")
+#plt.show()
+plt.savefig('word.png', bbox_inches='tight')
