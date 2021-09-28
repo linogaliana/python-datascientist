9b7734bcd22d5308356939efb71369b5153ec533
Romain Avouac <43444134+avouacr@users.noreply.github.com>
2021-09-28 17:29:33 +0200

    normalize paths

--- init_onyxia.sh
+++ init_onyxia.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git clone https://github.com/linogaliana/python-datascientist.git ~/python-datascientist
-cp -r ~/python-datascientist/notebooks/course/* ~/work/
+cp -r ~/python-datascientist/notebooks/course/* /home/jovyan/work/
 chown -R jovyan:users /home/jovyan/work/
