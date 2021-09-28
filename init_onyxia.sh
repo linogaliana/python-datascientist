0f53d83135f9ce69f75db1a1812b7be7e8a7fb7b
avouacr <romain.avouac@insee.fr>
2021-09-28 15:03:53 +0000

    fix permission issue which prevented to save changes in notebooks

--- init_onyxia.sh
+++ init_onyxia.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+
 git clone https://github.com/linogaliana/python-datascientist.git ~/python-datascientist
 cp -r ~/python-datascientist/notebooks/course/* ~/work/
+chown -R jovyan:users /home/jovyan/work/
