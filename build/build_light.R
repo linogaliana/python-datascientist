a9160493923e11b49bdeb14b56b6537e2708f579
Lino Galiana <33896139+linogaliana@users.noreply.github.com>
2021-10-31 18:36:25 +0100

    Nettoyage partie API + Git (#170)
    * Relec ka 2210 (#167)
    
    * coquilles webscraping
    
    * coquilles regex
    
    * relecture du cours et TP sur git KA
    
    * relecture KA du TP sur les API
    
    * Update content/course/manipulation/04a_webscraping_TP.Rmd
    
    * Update content/course/manipulation/04c_API_TP.Rmd
    
    * Update content/course/manipulation/04c_API_TP.Rmd
    
    * Update content/course/manipulation/04c_API_TP.Rmd
    
    * Update content/course/manipulation/04c_API_TP.Rmd
    
    * Update content/course/manipulation/04c_API_TP.Rmd
    
    * Update content/course/manipulation/04c_API_TP.Rmd
    
    Co-authored-by: Lino Galiana <33896139+linogaliana@users.noreply.github.com>
    
    * Automated changes
    
    * Automated changes
    
    * Automated changes
    
    * plein de détails
    
    * encore des exemples
    
    * ajoute détails
    
    * Automated changes
    
    * Automated changes
    
    * limite taille print
    
    * update
    
    * Automated changes
    
    * Automated changes
    
    * ajoute une carte leaflet
    
    * Automated changes
    
    * Automated changes
    
    * corrige plot manquant
    
    * afficher folium
    
    * print status
    
    * Automated changes
    
    * Automated changes
    
    * Automated changes
    
    * stylise la carte
    
    * Automated changes
    
    * Automated changes
    
    * customize carte
    
    * Automated changes
    
    * Automated changes
    
    * citer XD
    
    * afficher l'image
    
    * retire de la liste
    
    * Automated changes
    
    * Automated changes
    
    * Automated changes
    
    * Automated changes
    
    * Automated changes
    
    * Stupid idea to give a variable the name "r" since this is how reticulate makes the passthrough between r and python
    
    see https://github.com/rstudio/reticulate/issues/914
    
    * Automated changes
    
    * Automated changes
    
    * retire image
    
    * Automated changes
    
    * Automated changes
    
    * asis
    
    * Automated changes
    
    * Automated changes
    
    * test compil partie git
    
    * une petite intro
    
    * détails
    
    * on arrange
    
    * copier coller utilitr
    
    * note authent https
    
    * des détails supplémentaires
    
    * eval FALSE
    
    * clean
    
    * premiers elements
    
    * ajoute des éléments
    
    * capture écran
    
    * des modifs
    
    * ajoute image git
    
    * explications
    
    * ajoute option store
    
    * plus de modifs
    
    * reorg
    
    * clean
    
    * partie solo finie
    
    * ajoute éléments dans la partie cours
    
    * ajout header jupytext !
    
    * Automated changes
    
    * Automated changes
    
    * update fichiers
    
    * mef markdown
    
    * change emplacement
    
    * Automated changes
    
    * mef
    
    * Automated changes
    
    * Automated changes
    
    * Automated changes
    
    * Automated changes
    
    * typo
    
    * Automated changes
    
    * Automated changes
    
    * affiche image masquée
    
    * Automated changes
    
    * Automated changes
    
    * révise partie cadavre exquis
    
    * Automated changes
    
    * Automated changes
    
    * corrige typo
    
    * shell
    
    * Automated changes
    
    * Automated changes
    
    * Automated changes
    
    * cette image est mieux
    
    * Automated changes
    
    * Automated changes
    
    Co-authored-by: Kim A <kim.antunez@ensae.fr>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- build/build_light.R
+++ build/build_light.R
@@ -6,7 +6,7 @@ library(reticulate)
 content_rmd <- readLines("diff.txt")
 content_rmd <- content_rmd[startsWith(content_rmd, "content/course")]
 content_rmd <- content_rmd[endsWith(content_rmd, ".Rmd")]
-content_rmd <- content_rmd[!grepl("/git/", content_rmd)]
+#content_rmd <- content_rmd[!grepl("/git/", content_rmd)]
 content_rmd <- content_rmd[!grepl("06a_exo_supp_webscraping.", content_rmd)]
 
 file.remove(
