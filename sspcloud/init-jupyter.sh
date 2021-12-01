1a210b05d3e164a4fe572280052e1eb59fe0321c
Romain Avouac <43444134+avouacr@users.noreply.github.com>
2021-12-01 11:04:22 +0100

    fix init script

--- sspcloud/init-jupyter.sh
+++ sspcloud/init-jupyter.sh
@@ -6,19 +6,21 @@ CHAPTER=$2
 WORK_DIR=/home/jovyan/work
 CLONE_DIR=${WORK_DIR}/repo-git
 COURSE_DIR=${CLONE_DIR}/notebooks/course
+FORMATION_DIR=${WORK_DIR}/formation
 
 # Clone course repository
 REPO_URL=https://github.com/linogaliana/python-datascientist.git
 git clone --depth 1 $REPO_URL $CLONE_DIR
 
-# Put relevant notebook in work
+# Put relevant notebook in formation dir
+mkdir $FORMATION_DIR
 cp ${COURSE_DIR}/${SECTION}/${CHAPTER}.ipynb ${FORMATION_DIR}/
 
 # Give write permissions
 chown -R jovyan:users $FORMATION_DIR
 
 # Remove course Git repository
-rm -r $CLONE_DIR WORK_DIR/${lost+found}
+rm -r $CLONE_DIR
 
 # Open the relevant notebook when starting Jupyter Lab
 echo "c.LabApp.default_url = '/lab/tree/formation/${CHAPTER}.ipynb'" >> /home/jovyan/.jupyter/jupyter_server_config.py
