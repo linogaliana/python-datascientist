1022c42d6b9c97f1f9d49e04614aa6665c885638
antuki <kim.antunez@ensae.fr>
2021-12-11 18:15:01 +0100

    v0 de get_data pour partie NLP

---
+++ content/course/NLP/get_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def create_train_dataframes():
+    
+  Path("data").mkdir(parents=True, exist_ok=True)
+  
+  url='https://github.com/GU4243-ADS/spring2018-project1-ginnyqg/raw/master/data/spooky.csv'
+  train = pd.read_csv(url, encoding='latin-1')
+  train.columns = train.columns.str.capitalize()
+  train['ID'] = train['Id'].str.replace("id","")
+  train = train.set_index('Id')
+  
+  return train
