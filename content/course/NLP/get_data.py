03cea0b2e78e4e1c6ef3a1ece24a1ff36f7022d8
antuki <kim.antunez@ensae.fr>
2021-12-11 18:17:51 +0100

    get_data modif

--- content/course/NLP/get_data.py
+++ content/course/NLP/get_data.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
 def create_train_dataframes():
-    
-  Path("data").mkdir(parents=True, exist_ok=True)
-  
+
   url='https://github.com/GU4243-ADS/spring2018-project1-ginnyqg/raw/master/data/spooky.csv'
   train = pd.read_csv(url, encoding='latin-1')
   train.columns = train.columns.str.capitalize()
