5241c71b3027ad54f7c15888c91d06fb8ffd5575
linogaliana <linogaliana@yahoo.fr>
2022-05-25 15:56:16 +0000

    ajoute les headers

--- build/tweak_render.py
+++ build/tweak_render.py
@@ -7,11 +7,9 @@ with open('diff') as f:
     lines = f.read().splitlines() 
 
 lines = [l for l in lines if l.endswith('.qmd') ]
+lines += [f"content/course/{dir}/index.md" for dir in ["manipulation","visualisation","modelisation"]]
 
-if len(lines) == 0:
-    print("no new file")
-else:
-    config['project']['render'] = lines
+config['project']['render'] = lines
 
 with open('_quarto.yml', 'w') as outfile:
     yaml.dump(config, outfile, default_flow_style=False)
