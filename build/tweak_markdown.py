7b9f27be536bee06ddb26a5cd99bffdb89578e11
Lino Galiana <33896139+linogaliana@users.noreply.github.com>
2022-06-03 17:05:15 +0200

    Essaie régler les problèmes widgets JS (#231)
    * teste raw html
    
    * Automated changes
    
    * Automated changes
    
    * test avec rawhtml
    
    * Automated changes
    
    * Automated changes
    
    * un autre tp folium
    
    * Automated changes
    
    * Automated changes
    
    * test intégration plotyl
    
    * Automated changes
    
    * Automated changes
    
    * try rawhtml
    
    * Automated changes
    
    * Automated changes
    
    * chart
    
    * Automated changes
    
    * Automated changes
    
    * up
    
    * Automated changes
    
    * Automated changes
    
    * try diff shortcode
    
    * Automated changes
    
    * Automated changes
    
    * try diff shortcode
    
    * head plotly
    
    * Automated changes
    
    * Automated changes
    
    * Automated changes
    
    * tente basique
    
    * Automated changes
    
    * Automated changes
    
    * try as html
    
    * Automated changes
    
    * Automated changes
    
    * test show
    
    * Automated changes
    
    * Automated changes
    
    * regex replace header
    
    * ajoute chart plotly
    
    * Automated changes
    
    * Automated changes
    
    * add content
    
    * Automated changes
    
    * Automated changes
    
    * change ordre
    
    * Automated changes
    
    * up
    
    * not _index
    
    * Automated changes
    
    * Automated changes
    
    * not _index
    
    * Automated changes
    
    * retire
    
    * Automated changes
    
    * retire
    
    * Automated changes
    
    * retire
    
    * Automated changes
    
    * Automated changes
    
    * tweak all
    
    * Automated changes
    
    * essaie diff
    
    * Automated changes
    
    * encoding
    
    * Automated changes
    
    * retire espace
    
    * Automated changes
    
    * Automated changes
    
    * plotly
    
    * Automated changes
    
    * Automated changes
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- build/tweak_markdown.py
+++ build/tweak_markdown.py
@@ -1,10 +1,11 @@
 import glob
 import re
+
 def read_file(filename):
     with open(filename, 'r', encoding='utf-8') as f:
         text = f.readlines()
         f.close()
-    new_text = " ".join([line for line in text])
+    new_text = "".join([line for line in text])
     s = new_text
     return s
 
@@ -22,14 +23,20 @@ def tweak_js_plotly(content):
 
 
 def write_file(filename, content):
-    with open(filename, 'w') as f:
+    with open(filename, 'w', encoding="utf-8") as f:
         f.write(content)
 
 def clean_write_file(fl):
     content = read_file(fl)
-    if not fl.endswith("_index.md"):
-        content = replace_shortcode_tabs(content)
-    write_file(fl, content)
+    if re.search(
+        r'<script type="text/javascript">\n([\S\s]*)</script>\n',
+        content) is not None:
+        print("plotly detected")
+        content = tweak_js_plotly(content)
+        write_file(fl, content)
+    else:
+        print("nothing to do")
 
-list_files = glob.glob("./content/**/*.md", recursive=True)
-[clean_write_file(fl) for fl in list_files]
+list_files = glob.glob("./content/course/**/*.md", recursive=True)
+print(list_files)
+[clean_write_file(fl) for fl in list_files if not fl.endswith("_index.md")]
