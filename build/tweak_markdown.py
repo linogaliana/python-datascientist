9f309850a0559afa0311b0a599387c6bafbd7561
linogaliana <linogaliana@yahoo.fr>
2022-06-09 16:47:26 +0000

    test tweak markdown

--- build/tweak_markdown.py
+++ build/tweak_markdown.py
@@ -28,14 +28,22 @@ def write_file(filename, content):
 
 def clean_write_file(fl):
     content = read_file(fl)
-    if re.search(
-        r'<script type="text/javascript">\n([\S\s]*)</script>\n',
-        content) is not None:
-        print("plotly detected")
-        content = tweak_js_plotly(content)
-        write_file(fl, content)
+    add_text = """
+    <script src="https://d3js.org/d3.v7.min.js"></script>
+    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+    """
+    if re.search("plotly: true", content) is not None:
+        yaml, text = content.split('---\n', 2)[1:]
+        content = "---\n"+ yaml + "---\n" \
+            + add_text+"\n"+text
     else:
         print("nothing to do")
+#    if re.search(
+#        r'<script type="text/javascript">\n([\S\s]*)</script>\n',
+#        content) is not None:
+#        print("plotly detected")
+#        content = tweak_js_plotly(content)
+#        write_file(fl, content)
 
 list_files = glob.glob("./content/course/**/*.md", recursive=True)
 print(list_files)
