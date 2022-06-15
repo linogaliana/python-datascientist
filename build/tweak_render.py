874c5d53cedb8ad434d0d8593182937fb5efb061
Lino Galiana <linogaliana@yahoo.fr>
2022-06-15 18:40:46 +0200

    add custom

--- build/tweak_render.py
+++ build/tweak_render.py
@@ -8,6 +8,7 @@ with open('diff') as f:
 
 lines = [l for l in lines if l.endswith('.qmd') ]
 lines += [f"content/course/{dir}/index.qmd" for dir in ["manipulation","visualisation","modelisation"]]
+lines += [f"content/course/index.qmd"]
 
 config['project']['render'] = lines
 
