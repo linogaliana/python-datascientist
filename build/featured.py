95ad5f1892ef1f56e9b0dade2256b27273ca1db1
Lino Galiana <33896139+linogaliana@users.noreply.github.com>
2022-08-05 16:47:22 +0200

    Pimp la _homepage_ again ✨ ✨  (#253)
    * script gif
    
    * nicer
    
    * nicer
    
    * error
    
    * Automated changes
    
    * Automated changes
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- build/featured.py
+++ build/featured.py
@@ -1,6 +1,10 @@
 import yaml
 import os
 import shutil
+import glob
+import imageio.v2 as imageio
+
+
 from PIL import Image
 
 #os.chdir("python-datascientist/")
@@ -19,6 +23,14 @@ def resize_write_image(dict_featured, location):
     loc = f"./content/course/{location}/{loc}"
     shutil.copy2(loc, os.path.dirname(loc) + "/featured.png")
     
+
+list_images = glob.glob("./content/course/**/**/featured.png")
+list_images = ["./content/home/word.png"] + list_images
+with imageio.get_writer('animation.gif', mode='I', fps = 1) as writer:
+    for filename in list_images:
+        image = imageio.imread(filename)
+        writer.append_data(image)
+
 
 #img = Image.open(loc)
 #img = img.resize((req_width,req_height), Image.ANTIALIAS)
