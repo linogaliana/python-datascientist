975679f55b02b91eb91ca5d7e8d1c57781f9fd90
linogaliana <linogaliana@yahoo.fr>
2022-08-23 07:45:56 +0000

    tweak md for notebooks

--- build/tweak_markdown.py
+++ build/tweak_markdown.py
@@ -46,22 +46,6 @@ def clean_write_file(fl):
     write_file(fl, content)
 
 
-def change_box_markdown(fl):
-    content = read_file(fl)
-    content = re.sub(r"(“|”)",'"',content)
-    print(f"File: {fl}")
-    list_rows = content.split("\n")
-    corresp_boxes = {
-        "note": "<div class=\"alert alert-info\" role=\"alert\">\n",
-        "danger": "<div class=\"alert alert-danger\" role=\"alert\">\n",
-        "exercise": "<div class=\"alert alert-success\" role=\"alert\">\n",
-        "hint": "<div class=\"alert alert-warning\" role=\"alert\">\n"}
-
-    tweak_md = [corresp_boxes[re.search('status=\"(.*?)\"',l).group(1)] if l.startswith("{{% box") else l for l in list_rows]
-    tweak_md = ["</div>" if l.startswith("{{% /box") else l for l in tweak_md]
-    tweak_md = "\n".join(tweak_md)
-    write_file("test.qmd", tweak_md)
-
 
 list_files = glob.glob("./content/course/**/index.md", recursive=True)
 print(list_files)
