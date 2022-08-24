fe4e40c6211f952a97bf705ac2e74aa35ad28e0c
linogaliana <linogaliana@yahoo.fr>
2022-08-24 08:51:53 +0000

    try christophe's solution

--- build/pimp_notebook.py
+++ build/pimp_notebook.py
@@ -15,14 +15,14 @@ def change_box_markdown(fl):
 #        "exercise": "::: {.alert .alert-success}\n",
 #        "hint": "::: {.alert .alert-warning}\n"}
     corresp_boxes = {
-        "note": "```{=html}\n<div class=\"alert alert-info\" role=\"alert\">",
-        "warning": "```{=html}\n<div class=\"alert alert-danger\" role=\"alert\">",
-        "danger": "```{=html}\n<div class=\"alert alert-danger\" role=\"alert\">",
-        "exercise": "```{=html}\n<div class=\"alert alert-success\" role=\"alert\">",
-        "hint": "```{=html}\n<div class=\"alert alert-warning\" role=\"alert\">"}
+        "note": "::: {.cell .markdown}\n```{=html}\n<div class=\"alert alert-info\" role=\"alert\">\n```",
+        "warning": "::: {.cell .markdown}\n```{=html}\n<div class=\"alert alert-danger\" role=\"alert\">\n```",
+        "danger": "::: {.cell .markdown}\n```{=html}\n<div class=\"alert alert-danger\" role=\"alert\">\n```",
+        "exercise": "::: {.cell .markdown}\n```{=html}\n<div class=\"alert alert-success\" role=\"alert\">\n```",
+        "hint": "::: {.cell .markdown}\n```{=html}\n<div class=\"alert alert-warning\" role=\"alert\">\n```"}
     tweak_md = [corresp_boxes[re.search('status=\"(.*?)\"',l).group(1)] if l.startswith("{{% box") else l for l in list_rows]
     #tweak_md = [":::" if l.startswith("{{% /box") else l for l in tweak_md]
-    tweak_md = ["</div>\n```" if l.startswith("{{% /box") else l for l in tweak_md]
+    tweak_md = ["```{=html}\n</div>\n```" if l.startswith("{{% /box") else l for l in tweak_md]
     tweak_md = "\n".join(tweak_md)
     write_file(fl, tweak_md)
 
