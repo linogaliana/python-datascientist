ce46f9022be80a569a0e5dc4c121ad5590e1d4b5
Lino Galiana <33896139+linogaliana@users.noreply.github.com>
2022-08-25 08:30:22 +0200

    Adaptation du shortcode spoiler en notebook (#257)
    * Spoiler
    
    * Pimp notebooks
    
    * badges
    
    * Automated changes
    
    * Automated changes
    
    * les trois points
    
    * Automated changes
    
    * Automated changes
    
    * format
    
    * format
    
    * Automated changes
    
    * Automated changes
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- build/pimp_notebook.py
+++ build/pimp_notebook.py
@@ -16,8 +16,11 @@ def change_box_markdown(fl):
     content = read_file(fl)
     content = re.sub(r"(“|”)",'"',content)
     print(f"File: {fl}")
-    # BOXES
+    # SPOILER !
     list_rows = content.split("\n")
+    list_rows = [l.replace("{{< spoiler text=\"","::: {.cell .markdown}\n```{=html}\n<details>\n<summary>").replace('\" >}}',"</summary>\n```\n") if l.startswith("{{< spoiler") else l for l in list_rows]
+    list_rows = [l.replace("{{< /spoiler >}}", "\n```{=html}\n</details>\n```\n:::") if l.startswith("{{< /spoiler") else l for l in list_rows]
+    # BOXES
     corresp_boxes = {
         "note": "::: {.cell .markdown}\n```{=html}\n<div class=\"alert alert-info\" role=\"alert\">\n```",
         "warning": "::: {.cell .markdown}\n```{=html}\n<div class=\"alert alert-danger\" role=\"alert\">\n```",
