c8fd281ccb45ce1b673932293f68c18851cf7213
avouacr <avouacr@pm.me>
2022-09-02 08:55:13 +0000

    abandon CI to automatically document course on SSP Cloud training section

--- sspcloud/generate-doc.py
+++ sspcloud/generate-doc.py
@@ -1,5 +1,4 @@
 """Generate doc of a training program for https://www.sspcloud.fr."""
-import logging
 import os
 from os.path import dirname
 import json
@@ -19,11 +18,11 @@ def extract_metadata_md(md_path):
 def generate_block(name, abstract, authors, types, tags, category,
                    img_url, article_url=None, deployment_url=None):
     """Generate json documentation for a training program block.
-    
+
     If both `article_url` and `deployment_url` are None, the block is assumed
     to be a container for further sub-blocks (i.e. has an "open" button).
     Otherwise, either one of or both `article_url` and `deployment_url` must
-    be provided. 
+    be provided.
     """
     block = {
         "name": name,
@@ -53,7 +52,7 @@ if __name__ == "__main__":
         md = json.load(file)
 
     # Main URLs
-    LAUNCHER_TMPLT = ("https://datalab.sspcloud.fr/launcher/inseefrlab-helm-charts-datascience/jupyter"
+    LAUNCHER_TMPLT = ("https://datalab.sspcloud.fr/launcher/ide/jupyter"
                       "?autoLaunch=true&onyxia.friendlyName=%C2%ABpython-datascience%C2%BB"
                       "&init.personalInit=%C2%ABhttps%3A%2F%2Fraw.githubusercontent.com%2Flinogaliana%2Fpython-datascientist%2Fmaster%2Fsspcloud%2Finit-jupyter.sh%C2%BB"
                       "&init.personalInitArgs=%C2%AB{init_args}%C2%BB"
@@ -61,12 +60,12 @@ if __name__ == "__main__":
     COURSE_NAME_ENCODED = urllib.parse.quote(md['name'])
 
     # Build documentation's top block
-    doc_json = generate_block(name=md["name"], 
-                              abstract=md["abstract"], 
-                              authors=md["authors"], 
-                              types=md["types"], 
-                              tags=md["tags"], 
-                              category=md["category"], 
+    doc_json = generate_block(name=md["name"],
+                              abstract=md["abstract"],
+                              authors=md["authors"],
+                              types=md["types"],
+                              tags=md["tags"],
+                              category=md["category"],
                               img_url=md["imageUrl"]
                               )
     for section in md["sections"].keys():
@@ -75,7 +74,7 @@ if __name__ == "__main__":
         section_doc = generate_block(name=section_md["name"],
                                      abstract=section_md["abstract"],
                                      authors=md["authors"],
-                                     types=md["types"], 
+                                     types=md["types"],
                                      tags=md["tags"],
                                      category=md["category"],
                                      img_url=md["imageUrl"]
@@ -92,14 +91,14 @@ if __name__ == "__main__":
                     launcher_url = LAUNCHER_TMPLT.format(init_args=init_args)
 
                     chapter_doc = generate_block(name=name,
-                                                abstract=abstract,
-                                                authors=md["authors"],
-                                                types=md["types"],
-                                                tags=md["tags"],
-                                                category=md["category"],
-                                                img_url=md["imageUrl"],
-                                                deployment_url=launcher_url
-                                                )
+                                                 abstract=abstract,
+                                                 authors=md["authors"],
+                                                 types=md["types"],
+                                                 tags=md["tags"],
+                                                 category=md["category"],
+                                                 img_url=md["imageUrl"],
+                                                 deployment_url=launcher_url
+                                                 )
                     section_doc["parts"].append(chapter_doc)
 
                 else:
@@ -109,6 +108,4 @@ if __name__ == "__main__":
 
     # Export to json
     with open(os.path.join(PROJECT_DIR, "doc.json"), "w") as json_file:
-        json.dump(doc_json, json_file, 
-        # indent=4  # For test only
-        )
+        json.dump(doc_json, json_file, indent=2, ensure_ascii=False)
