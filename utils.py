785207eda996c11797f5a428fa59077049ca9e2d
Lino Galiana <33896139+linogaliana@users.noreply.github.com>
2022-09-19 18:44:34 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -55,7 +55,7 @@ def reminder_badges(
         nbviewer_link = f'<a href="{nbviewer_link}" target="_blank" rel="noopener">'\
             '<img src="https://img.shields.io/badge/Visualize-nbviewer-blue?logo=Jupyter" alt="nbviewer"></a>'
 
-    onyxia_link_launcher = "https://datalab.sspcloud.fr/launcher/ide/jupyter"\
+    onyxia_link_launcher = "https://datalab.sspcloud.fr/launcher/ide/jupyter-python"\
         "?autoLaunch=true&onyxia.friendlyName=%C2%ABpython-datascience%C2%BB"\
         "&init.personalInit=%C2%ABhttps%3A%2F%2Fraw.githubusercontent.com%2Flinogaliana%2Fpython-datascientist%2Fmaster%2Fsspcloud%2Finit-jupyter.sh%C2%BB"\
         f"&init.personalInitArgs=%C2%AB{section}/{chapter}%C2%BB&security.allowlist.enabled=false"
