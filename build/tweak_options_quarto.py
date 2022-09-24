9dd4ba1c906c130da923e2aa9f3ca8b8f88d1b6a
linogaliana <linogaliana@yahoo.fr>
2022-09-24 12:40:22 +0000

    overwrite echo false

---
+++ build/tweak_options_quarto.py
@@ -0,0 +1,18 @@
+import glob
+import re
+
+from tweak_markdown import read_file, write_file
+
+with open('diff') as f:
+    lines = f.read().splitlines() 
+
+list_files = [l for l in lines if l.endswith('.qmd')]
+
+def clean_overwrite_file(fl):
+    content = read_file(fl)
+    content = re.sub("#|echo: false", "", content)
+    content = re.sub("#| echo: false", "", content)
+    write_file(fl, content)
+
+if __name__ == '__main__':
+    [clean_overwrite_file(fl)]
