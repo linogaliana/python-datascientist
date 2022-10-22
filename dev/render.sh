f9669f9901e9d0dd6e009b17766d167087b3d326
avouacr <avouacr@pm.me>
2022-10-22 12:57:22 +0000

    dev tools

--- dev/render.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-hugo server -p 5000 --bind 0.0.0.0
--- dev/set-up.sh
+++ dev/set-up.sh
@@ -9,14 +9,18 @@ wget https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz -O go.tar.gz && \
 echo "PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile
 
 # Install hugo
-HUGO_VERSION="0.104.3" 
-wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux-amd64.deb -O hugo.deb && \
+HUGO_VERSION="0.97.3" 
+wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.deb -O hugo.deb && \
     sudo dpkg -i hugo.deb && \
     sudo rm hugo.deb
 
+# Install quarto
 QUARTO_VERSION="1.1.251"
 wget "https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.deb" -O quarto.deb && \
     sudo dpkg -i quarto.deb && \
     sudo rm quarto.deb
+
+# Install needed Python packages
+pip install wordcloud 
 
 # source $HOME/.profile
