f15c98286f60eca35d9f2aff060a3f79d34971a1
avouacr <avouacr@pm.me>
2022-10-22 14:48:18 +0000

    dev tools

--- dev/render-changes.sh
+++ dev/render-changes.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+git diff --name-only >> diff
+python build/tweak_render.py
+python build/pimp_notebook.py
+
 quarto render --to hugo
 find . -name "index.md" | rename -f -d 's/^/_/'
 
--- dev/set-up.sh
+++ dev/set-up.sh
@@ -6,7 +6,7 @@ wget https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz -O go.tar.gz && \
     sudo rm -rf /usr/local/go && \
     sudo tar -C /usr/local -xzf go.tar.gz && \
     sudo rm go.tar.gz
-echo "PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile
+echo "PATH=$PATH:/usr/local/go/bin" >> $HOME/.bashrc
 
 # Install hugo
 HUGO_VERSION="0.97.3" 
@@ -19,8 +19,5 @@ QUARTO_VERSION="1.1.251"
 wget "https://github.com/quarto-dev/quarto-cli/releases/download/v${QUARTO_VERSION}/quarto-${QUARTO_VERSION}-linux-amd64.deb" -O quarto.deb && \
     sudo dpkg -i quarto.deb && \
     sudo rm quarto.deb
-
-# Install needed Python packages
-pip install wordcloud 
 
 # source $HOME/.profile
