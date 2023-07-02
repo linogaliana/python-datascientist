a1baca8948f948df5c00447dbadf80143fcd73a1
Lino Galiana <lino.galiana@insee.fr>
2023-07-02 10:58:15 +0000

    Pipeline Quarto & Pages 🚀 (#365)
    * initialisation quarto
    
    * Cards
    
    * navigation
    
    * modern datascience
    
    * Homepage
    
    * prepare build
    
    * quarto
    
    * Automated changes
    
    * Automated changes
    
    * Automated changes
    
    * Automated changes
    
    * Automated changes
    
    * Automated changes
    
    * github action
    
    * Automated changes
    
    * Automated changes
    
    * GHA email
    
    * Automated changes
    
    * Automated changes
    
    * intro
    
    * Automated changes
    
    * Automated changes
    
    * python introduction
    
    * change link
    
    * utils
    
    * test eval true
    
    * eval false webscraping
    
    * remove index
    
    * restore file
    
    * Automated changes
    
    * Automated changes
    
    * eval false
    
    * Automated changes
    
    * Automated changes
    
    * retour README
    
    * Automated changes
    
    * Automated changes
    
    * eval false elastic
    
    * Automated changes
    
    * Automated changes
    
    * featured manipulation
    
    * Automated changes
    
    * Automated changes
    
    * featued false
    
    * Automated changes
    
    * Automated changes
    
    * harmonise
    
    * NLP
    
    * Automated changes
    
    * Automated changes
    
    * rm
    
    * css styling
    
    * quarto
    
    * Automated changes
    
    * Automated changes
    
    * css
    
    * Automated changes
    
    * theme
    
    * Automated changes
    
    * Automated changes
    
    * custom css
    
    * Automated changes
    
    * Automated changes
    
    * tweak project
    
    * Force website project when using notebooks
    
    * hack names
    
    * tweak project
    
    * remove old file
    
    * featured
    
    * Automated changes
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- build/tweak_render.py
+++ build/tweak_render.py
@@ -7,9 +7,13 @@ with open('diff') as f:
     lines = f.read().splitlines() 
 
 lines = [l for l in lines if l.endswith('.qmd') ]
-lines += [f"content/course/{dir}/index.qmd" for dir in ["manipulation","visualisation","modelisation"]]
+lines += [f"content/course/{dir}/index.qmd" \
+    for dir in ["manipulation", "visualisation", "modelisation", "NLP", "modern-ds"]
+    ]
 lines += [f"content/course/index.qmd"]
 lines += [f"!content/slides/intro/index.qmd"]
+lines += [f"index.qmd"]
+
 
 config['project']['render'] = lines
 
--- go.mod
+++
@@ -1,9 +0,0 @@
-module github.com/wowchemy/starter-hugo-online-course
-
-go 1.19
-
-require (
-	github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-plugin-netlify v1.0.0 // indirect
-	github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-plugin-netlify-cms v1.0.0 // indirect
-	github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy/v5 v5.7.0 // indirect
-)
