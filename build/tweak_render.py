ed8153150472071dc19813f54568f1f82d66fe8f
Lino Galiana <lino.galiana@insee.fr>
2023-07-03 09:55:24 +0000

    Hero banner (#366)
    * Hero banner
    
    * Automated changes
    
    * Automated changes
    
    * list files in master branch
    
    * branch deploy
    
    * Automated changes
    
    * Automated changes
    
    * Automated changes
    
    * Automated changes
    
    * netlify dic
    
    * Automated changes
    
    * Automated changes
    
    * print lines
    
    * Automated changes
    
    * Automated changes
    
    * change directory
    
    * Automated changes
    
    * Automated changes
    
    * try from book
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- build/tweak_render.py
+++ build/tweak_render.py
@@ -1,10 +1,15 @@
+import os
+import glob
 import yaml
 
 with open("_quarto.yml", "r") as stream:
     config = yaml.load(stream, Loader=yaml.FullLoader)
 
-with open('diff') as f:
-    lines = f.read().splitlines() 
+if os.path.exists('diff'):
+    with open('diff') as f:
+        lines = f.read().splitlines()
+else:
+    lines = glob.glob('content/**/*.qmd', recursive=True)
 
 lines = [l for l in lines if l.endswith('.qmd') ]
 lines += [f"content/course/{dir}/index.qmd" \
@@ -14,10 +19,10 @@ lines += [f"content/course/index.qmd"]
 lines += [f"!content/slides/intro/index.qmd"]
 lines += [f"index.qmd"]
 
-
+print(lines)
 config['project']['render'] = lines
 
-with open('_quarto.yml', 'w') as outfile:
+with open('_quarto_temp.yml', 'w') as outfile:
     yaml.dump(config, outfile, default_flow_style=False)
 
 print("Done")
