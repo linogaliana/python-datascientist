2d1cf4885cd32ae26ac13baca9a7aab3f2d762ff
Lino Galiana <lino.galiana@insee.fr>
2023-08-25 11:23:02 +0200

    Simplification de la structure 🤓 (#393)
    * Change structure
    
    * restructure
    
    * Simplify again structure
    
    * Rename files
    
    * change sys structure
    
    * badges
    
    * tweak render
    
    * rm files
    
    * rm functions not used
    
    * Changing structure

--- dev-scripts/render-chapter.sh
+++ dev-scripts/render-chapter.sh
@@ -3,7 +3,7 @@
 SECTION=$1
 CHAPTER=$2
 
-SECTION_PATH="content/course/${SECTION}"
+SECTION_PATH="content/${SECTION}"
 CHAPTER_PATH="${SECTION_PATH}/${CHAPTER}"
 
 quarto preview -port 5000 --host 0.0.0.0
