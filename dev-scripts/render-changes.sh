7091e5939d23648380017b4d0e2b545b10e10065
linogaliana <lino.galiana@insee.fr>
2023-10-18 15:28:08 +0000

    Retire quelques scripts devenus obsolètes

--- dev-scripts/render-changes.sh
+++ dev-scripts/render-changes.sh
@@ -3,11 +3,9 @@
 git diff --name-only >> diff
 python build/tweak_render.py
 python build/pimp_notebook.py
-if [ "$1" = "correction" ]; then
-    python build/tweak_headers_quarto.py
-fi
+
 
 quarto render --to hugo
 find . -name "index.md" | rename -f -d 's/^/_/'
 
-hugo server -p 5000 --bind 0.0.0.0
+quarto preview --port 5000 --host 0.0.0.0
