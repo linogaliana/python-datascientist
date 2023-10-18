f6e2eb75a89916f79e3b4a36a1c9b2918586aca9
linogaliana <lino.galiana@insee.fr>
2023-10-18 15:28:08 +0000

    Retire quelques scripts devenus obsolètes

--- dev-scripts/build-correction-nb.sh
+++ dev-scripts/build-correction-nb.sh
@@ -3,7 +3,6 @@
 git diff --name-only >> diff
 python build/tweak_render.py
 python build/pimp_notebook.py
-python build/tweak_headers_quarto.py
 
 quarto render content --to ipynb --execute
 mkdir -p corrections
