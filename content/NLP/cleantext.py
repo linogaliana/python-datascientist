4f41cf6a768cd76595b0109bd1b04f903e9599fa
Lino Galiana <lino.galiana@insee.fr>
2024-06-14 15:00:41 +0200

    Une partie sur les sacs de mots plus cohérente (#501)
    * up
    
    * intro
    
    * Nettoyage
    
    * Ajoute des éléments
    
    * table
    
    * include_dumas
    
    * save old
    
    * modularise
    
    * clean
    
    * render
    
    * sépare en 2*
    
    * dumas
    
    * fasttext

---
+++ content/NLP/cleantext.py
@@ -0,0 +1,7 @@
+# Function to clean the text
+def clean_text(doc):
+    # Tokenize, remove stop words and punctuation, and lemmatize
+    cleaned_tokens = [token.lemma_ for token in doc if not token.is_stop and not token.is_punct]
+    # Join tokens back into a single string
+    cleaned_text = ' '.join(cleaned_tokens)
+    return cleaned_text
