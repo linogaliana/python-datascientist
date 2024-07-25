2f4d6caacf92f273e2a2996cdfabf4c0f7bf956e
linogaliana <lino.galiana@insee.fr>
2024-07-25 17:01:15 +0000

    version

--- build/lang-notebook.lua
+++ build/lang-notebook.lua
@@ -1,4 +1,20 @@
 local filename = quarto.doc.input_file
+local dirname = quarto.project.directory
+
+-- Escape special characters in dirname
+local function escape_pattern(str)
+    return str:gsub("([^%w])", "%%%1")
+end
+
+dirname = escape_pattern(dirname)
+
+local filename_relative = filename:gsub("^" .. dirname, "")
+filename_relative = "https://pythonds.linogaliana.fr" .. filename_relative
+filename_relative = filename_relative:gsub(".qmd", ".html")
+
+quarto.log.output("Directory name: " .. dirname)
+quarto.log.output("Filename: " .. filename)
+quarto.log.output("Relative filename: " .. filename_relative)
 
 -- Function to check if string ends with a specific suffix
 local function ends_with(str, suffix)
@@ -6,27 +22,48 @@ local function ends_with(str, suffix)
 end
 
 -- Determine the language based on the filename suffix
-local function get_text_language(filename)
-    quarto.log.output("Checking filename: " .. filename)  -- Debug print
+local function get_text_language(filename_relative)
     local text
+    -- Add html formatting around
+    local headBox = "<div class=\"alert alert-info\" role=\"alert\">\n" ..
+        "<h3 class=\"alert-heading\">Version</h3>\n"
 
-    if ends_with(filename, "_en.qmd") then
-        text = "This is the English version of that chapter, to see French version go [there](" .. filename .. ")"
+    if ends_with(filename_relative, "_en.html") then
+        text = "This is the English 🇬🇧 🇺🇸 version of that chapter," ..
+            "to see the French version go " ..
+            "<a href=\"" .. filename_relative:gsub("_en.html", ".html") ..
+            "\">there</a>"
         quarto.log.output("Language detected: english")  -- Debug print
-        return text
     else
-        text = "This is the English version of that chapter, to see French version go [there](" .. filename .. ")"
+        text = "This is the French version 🇫🇷 of that chapter," ..
+            "to see the English version go " ..
+            "<a href=\"" .. filename_relative:gsub("_fr.html", "_en.html") ..
+            "\">there</a>"
         quarto.log.output("Language detected: french")  -- Debug print
-        return text
     end
+
+    text = headBox .. text .. "\n</div>"
+
+    return text
 end
+
+-- Get the language-specific text
+local text_language = get_text_language(filename_relative)
 
 if quarto.doc.is_format("ipynb") then
     return {
-        ["print-warning-language"] = function(args, kwargs)
-            local text = get_text_language(filename)
-            quarto.log.output("Final output: " .. text)  -- Debug print
-            return text
-        end
-    }
+        {
+          Para = function (elem)
+            for i, v in ipairs(elem.content) do
+                if v.text == "{{helloworld}}" then
+                    return pandoc.Div(
+                        pandoc.RawBlock("html", text_language),
+                        {class = "markdown"}
+                    )
+                end
+            end
+            return elem
+          end,
+        }
+      }    
 end
