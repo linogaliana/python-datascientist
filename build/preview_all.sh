c0afc6a786bb2f052c4abd92ee7e8356ad8959a1
linogaliana <lino.galiana@insee.fr>
2024-08-07 16:56:36 +0000

    format

--- build/preview_all.sh
+++ build/preview_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-quarto render --profile fr
-quarto render --profile en
+quarto render --profile fr --to html
+quarto render --profile en --to html
 cd _site/
 python3 -m http.server -b 0.0.0.0 5000
