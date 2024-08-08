5992e207082c6c93313728d1be2a850e031f82d6
linogaliana <lino.galiana@insee.fr>
2024-08-08 13:08:56 +0000

    move files

--- build/move_files.py
+++ build/move_files.py
@@ -1,22 +1,33 @@
 import shutil
 import glob
-import sys
 import os
+import argparse
 
-direction = sys.argv[1]
+# Set up argument parsing
+parser = argparse.ArgumentParser(description="Copy .ipynb files from source to target directory")
+parser.add_argument(
+    "--direction",
+    help="The target direction to copy files to",
+    default="temp_notebooks/corrections"
+)
+args = parser.parse_args()
+
+direction = args.direction
 print(direction)
 
-list_files = glob.glob(f"_site/content/**/*.ipynb", recursive=True)
+# Find all .ipynb files in the specified directories
+list_files = glob.glob(f"_site/content/**/*.ipynb", recursive=True) + glob.glob(f"_site/en/content/**/*.ipynb", recursive=True)
 list_files = [fl for fl in list_files if not fl.endswith("_index.ipynb")]
-list_files = [fl for fl in list_files if fl.endswith("ipynb")]
 
 print(list_files)
 
 def hack_name(fl, direction):
-    newname = fl.replace("content", direction)
+    if 'en/content' in fl:
+        newname = fl.replace("en/content", f"{direction}/en")
+    else:
+        newname = fl.replace("content", direction)
     newname = newname.replace("_site", ".")
     return newname
-
 
 corresp = {f: hack_name(f, direction) for f in list_files}
 print(corresp)
@@ -27,4 +38,4 @@ internal_dirs = list(set(internal_dirs))
 
 [shutil.copy2(key, value) for key, value in corresp.items()]
 
-print("Done !")
+print("Done!")
