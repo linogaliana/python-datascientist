acb26e049a86dfc5e64cb0c6c4a1b34ef67cb121
lgaliana <lino.galiana@insee.fr>
2024-09-22 10:54:03 +0000

    Backup for Pandas

--- build/copy.sh
+++ build/copy.sh
@@ -4,3 +4,6 @@ mc anonymous set download s3/lgaliana/data/python-ENSAE
 
 wget -O dvf.parquet https://www.data.gouv.fr/fr/datasets/r/56bde1e9-e214-408b-888d-34c57ff005c4
 mc cp dvf.parquet s3/lgaliana/data/python-ENSAE/dvf.parquet
+
+wget -O cog_2023.csv https://www.insee.fr/fr/statistiques/fichier/6800675/v_commune_2023.csv
+mc cp cog_2023.csv s3/lgaliana/data/python-ENSAE/cog_2023.csv
