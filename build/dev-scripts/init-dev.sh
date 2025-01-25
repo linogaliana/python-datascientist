a60d750a740002bcbce0ba9994bafc54458794ae
lgaliana <lino.galiana@insee.fr>
2025-01-25 15:27:52 +0000

    init

---
+++ build/dev-scripts/init-dev.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Install VSCode personal parameters ---------
+
+curl -sSL https://raw.githubusercontent.com/linogaliana/init-scripts/refs/heads/main/install-copilot.sh | bash
+
+# Install dependencies -----------
+
+git clone https://github.com/linogaliana/python-datascientist.git --single-branch --depth 1
+cd python-datascientist
+pip install uv
+uv pip install -r requirements.txt
+
+# Formatters and precommit
+
+pre-commit install
+pre-commit run --all-files
+
+# Run nbstripout installation command in the terminal
+echo "Running nbstripout --install..."
+nbstripout --install
