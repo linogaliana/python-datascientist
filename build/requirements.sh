09b2dd2e66113de1415b1fd5468052192301fe2b
Lino Galiana <lino.galiana@insee.fr>
2025-06-05 09:05:50 +0200

    Pipeline GHA avec uv plutôt que Docker et install system (#609)
    * petit mot sur l'entraînement de modèle
    
    * plus de docker, du GHA
    
    * update requirements sysyem
    
    * run as sudo
    
    * install quato
    
    * install quato
    
    * typo uv run
    
    * update all gha

---
+++ build/requirements.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get update && \
+    apt install -y graphviz
+    apt-get -y install wget && \
+    apt-get install -y git && \
+    apt-get install build-essential -y && \
+    apt-get install libmagic-dev -y && \
+    DEBIAN_FRONTEND=noninteractive apt install -y imagemagick && \
+    rm -rf /var/lib/apt/lists/*
