7b0b9d0533314c85af38c67fa830392cd27b7dac
lgaliana <lino.galiana@insee.fr>
2025-12-25 19:20:48 +0000

    add requirements

--- build/requirements.sh
+++ build/requirements.sh
@@ -6,5 +6,6 @@ apt-get update && \
     apt-get install -y git && \
     apt-get install build-essential -y && \
     apt-get install libmagic-dev -y && \
+    apt-get install libgdal-dev -y && \
     DEBIAN_FRONTEND=noninteractive apt install -y imagemagick && \
     rm -rf /var/lib/apt/lists/*
